403f96f67cd7e76a2ed2b6a6c4e13f19487213be
olayemi omolisa <olayemiomolisa@yahoo.com>
2026-03-01 18:42:18 +0100

    mongodb

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
 export const connectDB = async ()=> {
-    await mongoose.connect('mongodb+srv://olayemiomolisa:198080@cluster0.nvv1kit.mongodb.net/Justring').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://olayemiomolisa:omololami@cluster0.qjnhwuo.mongodb.net/Justring').then(()=>console.log("DB Connected"));
 
 }
